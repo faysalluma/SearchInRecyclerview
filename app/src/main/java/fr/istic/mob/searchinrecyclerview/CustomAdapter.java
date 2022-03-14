848c02ed7690566a8c3faf2dc9cd07e8d4e09ef4
Faysal SANDA <faysalgluma@gmail.com>
2022-03-14 21:14:06 +0100

    Initialise Repository

---
+++ app/src/main/java/fr/istic/mob/searchinrecyclerview/CustomAdapter.java
@@ -0,0 +1,59 @@
+package fr.istic.mob.searchinrecyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Belal on 6/6/2017.
+ */
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+
+    private ArrayList<String> names;
+
+    public CustomAdapter(ArrayList<String> names) {
+        this.names = names;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_layout, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.textViewName.setText(names.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return names.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewName;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+        }
+    }
+
+    //This method will filter the list
+    //here we are passing the filtered data
+    //and assigning it to the list with notifydatasetchanged method
+    public void filterList(ArrayList<String> filterdNames) {
+        this.names = filterdNames;
+        notifyDataSetChanged();
+    }
+}
